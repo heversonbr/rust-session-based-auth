b72ed9b3d6d92782bdb715b9e5d090ebbbf062bf
heversonbr <heversonbr@gmail.com>
2025-09-27 00:32:48 +0200

    minor fix

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,4 @@ fn main() {
     println!("Run examples with: ");
     println!("       1) cargo run --example minimal-session-auth-example");
     println!("       2) cargo run --example actix-session-auth-example");
-    
-  
 }
